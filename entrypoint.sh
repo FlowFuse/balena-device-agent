098614aa7d11ad9507cb584e1b3c4b4be438afb7
Ben Hardill <hardillb@gmail.com>
2023-11-16 11:09:07 +0000

    Add debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,12 @@ set -e
 hostname $BALENA_DEVICE_NAME_AT_INIT
 
 if [ -z "$FF_DEVICE_YML" ]; then
+  echo "FF_DEVICE_YML env var found"
+  echo "$FF_DEVICE_YML"
   if ! [ -f /opt/flowfuse-device/device.yml ]; then
+    echo "Writing file"
     echo $FF_DEVICE_YML | base64 -d > /opt/flowfuse-device/device.yml
+    cat /opt/flowfuse-device/device-yml
   else
     echo "Existing device.yml found"
   fi
