fdc176606a344183b652eb60181494275b77f8f0
Ben Hardill <hardillb@gmail.com>
2023-11-16 10:52:55 +0000

    Right file path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 hostname $BALENA_DEVICE_NAME_AT_INIT
 
 if [ -z "$FF_DEVICE_YML" ]; then
-  if ! [ -f /opt/flowfuse/device.yml ]; then
-    echo $FF_DEVICE_YML | base64 -d > /opt/flowfuse/device.yml
+  if ! [ -f /opt/flowfuse-device/device.yml ]; then
+    echo $FF_DEVICE_YML | base64 -d > /opt/flowfuse-device/device.yml
   else
     echo "Existing device.yml found"
   fi
