f044ad34e29ba2ba7d865ccdb69e298ccf5ea376
Ben Hardill <hardillb@gmail.com>
2023-11-16 10:50:51 +0000

    Make device.yml configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,14 @@ set -e
 
 hostname $BALENA_DEVICE_NAME_AT_INIT
 
+if [ -z "$FF_DEVICE_YML" ]; then
+  if ! [ -f /opt/flowfuse/device.yml ]; then
+    echo $FF_DEVICE_YML | base64 -d > /opt/flowfuse/device.yml
+  else
+    echo "Existing device.yml found"
+  fi
+else
+  echo "no device.yml env var provided"
+fi
+
 exec "$@"
